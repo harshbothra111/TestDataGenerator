5511f2bd1afc57b2fb38035d7b363e241acfa352
Harsh Bothra <harsh.love.619@gmail.com>
2023-09-24 04:22:47 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { faker } from '@faker-js/faker';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,111 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TestDataGenerator';
+  title = 'Test Data Generator';
+  noOfColumns = 0;
+  isCreated = false;
+  columnDetails: any = [];
+  categories: any = [];
+  selectedCategoryIndex: any;
+  @ViewChild('dataForm') form: NgForm;
+  submitted: boolean;
+  noOfRows = 1;
+  data:any = [];
+
+  constructor(private http: HttpClient,private spinner: NgxSpinnerService) {
+    this.http.get("assets/data/column-types.json").subscribe({
+      next: (response) => {
+        this.categories = response;
+      }
+    })
+  }
+
+  createColumns() {
+    this.columnDetails = [];
+    this.isCreated = this.noOfColumns > 0;
+    if (this.isCreated) {
+      for (let i = 0; i < this.noOfColumns; i++) {
+        this.columnDetails.push({
+          name: "",
+          category: "",
+          subCategory: "",
+          range: {
+            min: "",
+            max: ""
+          }
+        });
+      }
+    }
+  }
+
+  updateCategory(selectedCategory: string, columnIndex: number) {
+    const index = this.categories.findIndex((x: any) => x.name === selectedCategory);
+    if (index >= 0) {
+      this.columnDetails[columnIndex].subCategory = "";
+      this.selectedCategoryIndex = index;
+    }
+  }
+
+  generateData() {
+    this.submitted = true;
+    if (this.form.form.valid) {
+      this.spinner.show();
+      this.data = [];
+      for (let i = 0; i < this.noOfRows; i++) {
+        let data:any = {};
+        this.columnDetails.forEach((column:any) => {
+          data.name = column.name;
+          data.value = faker.company.name();
+        });
+        this.data.push(data);
+      }
+      this.downloadFile();
+      this.spinner.hide();
+    }
+  }
+
+  downloadFile() {
+    const filename = 'data.csv';
+    let csvData = this.convertToCSV();
+    let blob = new Blob(['\ufeff' + csvData],
+      { type: 'text/csv;charset=utf-8;' });
+    let dwldLink = document.createElement("a");
+    let url = URL.createObjectURL(blob);
+    let isSafariBrowser = navigator.userAgent.indexOf('Safari') !=
+      -1 && navigator.userAgent.indexOf('Chrome') == -1;
+    if (isSafariBrowser) {
+
+      // If Safari open in new window to
+      // save file with random filename.
+      dwldLink.setAttribute("target", "_blank");
+    }
+    dwldLink.setAttribute("href", url);
+    dwldLink.setAttribute("download", filename + ".csv");
+    dwldLink.style.visibility = "hidden";
+    document.body.appendChild(dwldLink);
+    dwldLink.click();
+    document.body.removeChild(dwldLink);
+  }
+
+  convertToCSV() {
+    let headerList = this.columnDetails.map((x:any) => x.name);
+    let array = this.data;
+    let str = '';
+    let row = '';
+
+    for (let index in headerList) {
+      row += headerList[index] + ',';
+    }
+    row = row.slice(0, -1);
+    str += row + '\r\n';
+    for (let i = 0; i < array.length; i++) {
+      let line = '';
+      for (let index in headerList) {
+        line += array[i]["value"].replace(/,/g,'') + ',';
+      }
+      line = line.slice(0, -1);
+      str += line + '\r\n';
+    }
+    return str;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from  '@angular/common/http';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NgxSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
